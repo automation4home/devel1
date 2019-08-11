60cb09529bb944ed430f57216d670902db2ac62f
root <root@devel1.local.domain.com>
2019-08-11 18:35:56 +0200

    add install hacs

---
+++ hacs_install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+mkdir custom_components
+cd custom_components
+git clone https://github.com/custom-components/hacs.git hacs_temp
+cd hacs_temp
+git checkout $(git describe --tags --always $(git rev-list --tags --max-count=1000) | grep -e "[0-9]\+\.[0-9]\+\.[0-9]\+$" | head -n 1)
+cd ../
+cp -r hacs_temp/custom_components/hacs hacs
+rm -R hacs_temp
