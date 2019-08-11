63850da6c06796b7f6bfb5ebdabb489db000a3bc
root <root@devel1.local.domain.com>
2019-08-11 20:01:51 +0200

    update

--- hacs_install.sh
+++ hacs_install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-mkdir custom_components
-cd custom_components
+mkdir /usr/share/hassio/homeassistant/custom_components
+cd /usr/share/hassio/homeassistant/custom_components
 git clone https://github.com/custom-components/hacs.git hacs_temp
 cd hacs_temp
 git checkout $(git describe --tags --always $(git rev-list --tags --max-count=1000) | grep -e "[0-9]\+\.[0-9]\+\.[0-9]\+$" | head -n 1)
