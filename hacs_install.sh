c95b81c2c14c08ae48c9b3cbaba72e135cab10d9
Misak <v.mikes@seznam.cz>
2019-08-11 20:40:57 +0200

    rm hacs install

--- hacs_install.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-mkdir /usr/share/hassio/homeassistant/custom_components
-cd /usr/share/hassio/homeassistant/custom_components
-git clone https://github.com/custom-components/hacs.git hacs_temp
-cd hacs_temp
-git checkout $(git describe --tags --always $(git rev-list --tags --max-count=1000) | grep -e "[0-9]\+\.[0-9]\+\.[0-9]\+$" | head -n 1)
-cd ../
-cp -r hacs_temp/custom_components/hacs hacs
-rm -R hacs_temp
